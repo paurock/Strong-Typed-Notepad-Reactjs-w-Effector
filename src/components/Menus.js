3b2d3cbe8e86d62702578c434520325f7badbaec
Roman Tselenko <38553025+paurock@users.noreply.github.com>
2019-05-10 18:20:33 +0300

    Changed Back Button

--- src/components/Menus.js
+++ src/components/Menus.js
@@ -2,19 +2,23 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { AddNoteForm, NoteList } from './Notes';
 import { Button } from 'antd';
-import backButton from '../images/backButton.png';
  
 const PageTemplate = ({ children, history, location, name }) => (
 	<section className="main-container">
 	<h1 className="page-title">/{name}</h1>
 		{children}
-		<span className="back-button"><Button className="back-button"><BackBtn  history={history} location={location} /></Button></span>
+		<span className="back-button">
+		{location.pathname === '/' ? null :<>
+		<Button className="back-button">
+		<BackBtn  history={history} location={location} />
+		</Button></>}
+		</span>
 	</section>
 );
 
 export const BackBtn = ({ history, location }) => (
 		<nav  onClick={() => history.goBack()}>
-			{location.pathname === '/' ? null : <img alt="<<Назад" src={backButton}/>}
+			{location.pathname === '/' ? null : "<< Назад" }
 		</nav>
 	
 );
